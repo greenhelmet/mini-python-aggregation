5ce312d6fc54a2d59be67d760de909371e7a0816
장영준 <90244158+greenhelmet@users.noreply.github.com>
2026-02-03 11:47:36 +0900

    Week3/day2 item api and tests (#8)
    * feat: implement item service, api, and layered test structure
    
    * feat: implement item service, api, and layered test structure

--- mini_fastapi/app/api/routers/items.py
+++ mini_fastapi/app/api/routers/items.py
@@ -1,14 +1,21 @@
 from typing import List
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 from app.schemas.item import Item, ItemCreate
-from app.services.item_service import get_items, create_item
+from app.services.item_service import get_items, create_item, get_item_by_id
 
 router = APIRouter()
 
 @router.get("/items/", response_model=List[Item])
 async def read_items():
     return get_items()
+
+@router.get("/items/{item_id}", response_model=Item)
+async def get_item_by_id_endpoint(item_id: str):
+    try:
+        return get_item_by_id(item_id)
+    except ValueError:
+        return HTTPException(status_code=404, detail="Item not found")
 
 @router.post("/items/", response_model=Item)
 async def create_item_endpoint(item: ItemCreate):
--- mini_fastapi/app/services/item_service.py
+++ mini_fastapi/app/services/item_service.py
@@ -3,13 +3,23 @@ from uuid import uuid4
 
 from app.schemas.item import Item, ItemCreate
 
-_items: Dict[str, Item] = {}
+_items: list[Item] = []
 
 def get_items() -> List[Item]:
-    return list(_items.values())
+    return _items
 
 def create_item(data: ItemCreate) -> Item:
     item_id = str(uuid4())
-    item = Item(id=item_id)
-    _items[item_id] = item
+    item = Item(
+        id=item_id,
+        name=data.name,
+    )
+    _items.append(item)
     return item
+
+def get_item_by_id(item_id: str) -> Item:
+    for item in _items:
+        if item.id == item_id:
+            return item
+    raise ValueError(f"Item not found: {item_id}")
+    
