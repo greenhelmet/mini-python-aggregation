01cd023cc7966e957010f880127053ce95dc4640
장영준 <jjh202hjj@naver.com>
2026-02-08 09:38:53 +0900

    st

--- mini_fastapi/app/schemas/item.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class Item(BaseModel):
-    id: str
-    item_name: str
-    
-class ItemCreate(BaseModel):
-    item_name: str
--- mini_fastapi/tests/conftest.py
+++
@@ -1,5 +0,0 @@
-import sys
-from pathlib import Path
-
-ROOT_DIR = Path(__file__).resolve().parents[1]
-sys.path.append(str(ROOT_DIR))
